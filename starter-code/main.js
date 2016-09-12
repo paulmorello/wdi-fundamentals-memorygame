965e186d8c60e2585c78a5c6e4f654ad3c87d2d5
paulmorello <generalassembly@Generals-MacBook-Air-2.local>
2016-09-12 18:38:30 +1000

    Added JS game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if(cardTwo === cardFour) {
+	window.alert("You found a match.");
+} else {
+	window.alert("Sorry, try again.");
+}
+
