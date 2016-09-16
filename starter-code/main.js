7075b44cb9a0cd855bb9b2f298a2275da7f2c9f5
paulmorello <generalassembly@Generals-MacBook-Air-2.local>
2016-09-16 16:01:29 +1000

    wdi fundamentals memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,58 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if(cardTwo === cardFour) {
-	window.alert("You found a match.");
-} else {
-	window.alert("Sorry, try again.");
+// if(cardTwo === cardFour) {
+// 	window.alert("You found a match.");
+// } else {
+// 	window.alert("Sorry, try again.");
+// }
+
+var gameboard = document.getElementById('gameboard');
+
+var card = document.createElement('div');
+card.className = 'card';
+
+var cardText = document.createTextNode("...");
+
+card.appendChild('board');
+
+for (var i = 0; i < gameboard.length; i++) {
+gameboard[i].className('board');
 }
+
+createBoard = function();
+
+for (var i = 0; i < createBoard.length; i++) {
+createBoard[i].insideHTML("...");
+}
+
+createBoard.appendChild('board');
+
+var cards = ['queen','queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+for (var i = 0; i < cards.length; i++) {
+	cardElement.setAttribute('data-card', cards[i]);
+}
+
+function = isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+	} else {
+		cardsInPlay = [];
+	}
+}
+
+cardElement.addEventListener('click', isTwoCards);
+cardElement.innerHTML = '<img src="../images/queen.jpg" alt="queen"/>';
+
+function = isMatch() {
+cardElement.innerHTML = "";
+}
+
+
+
+
 
